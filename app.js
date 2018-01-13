7ad9a3e0b841607a40f7cc50e191086bd60e197e
Fink Viktor <finkvi@gmail.com>
2018-01-13 16:02:20 +0000

    deploy

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(function(err, req, res, next) {
   res.send('error');
 });
 
+
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
   console.info("Shop started on " + (process.env.IP || "0.0.0.0") + ":" + (process.env.PORT || 3000));
 });
